f4315edf5d2186c695c2a267a646949cc466b262
xiaohuichu <2235921607@qq.com>
2018-08-31 13:49:24 +0800

    测试

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-print_r(__DIR__.'/../vendor/autoload.php'); echo "<hr/>";
+print_r(__DIR__); echo "<hr/>";
 print_r(__DIR__.'/../vendor/yiisoft/yii2/Yii.php');echo "<hr/>";
 print_r(__DIR__ . '/../config/web.php');
 die;
