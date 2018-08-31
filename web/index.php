a013e5230f97b2648225a30379a7402042b3ce0b
xiaohuichu <2235921607@qq.com>
2018-08-31 13:59:32 +0800

    122

--- web/index.php
+++ web/index.php
@@ -4,14 +4,14 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-print_r(dirname(__FILE__)); echo "<hr/>";
-print_r(__DIR__.'/../vendor/yiisoft/yii2/Yii.php');echo "<hr/>";
-print_r(__DIR__ . '/../config/web.php');
-die;
+//print_r(dirname(__FILE__)); echo "<hr/>";
+//print_r(__DIR__.'/../vendor/yiisoft/yii2/Yii.php');echo "<hr/>";
+//print_r(__DIR__ . '/../config/web.php');
+//die;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require dirname(__DIR__). '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php';
 
-$config = require __DIR__ . '/../config/web.php';
+$config = require dirname(__DIR__). '/config/web.php';
 
 (new yii\web\Application($config))->run();
